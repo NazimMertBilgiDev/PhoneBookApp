699a457b148eb7d3de6ccd96b7c78186b99f75d8
Nazım Mert BİLGİ <nmbjobs@outlook.com.tr>
2019-03-23 17:47:53 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello .net core!");
             Console.WriteLine("Hello all developers!");
-
+            // write code here.
         }
     }
 }
